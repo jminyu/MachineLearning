468b12cc8dbce8957af258e4492ba040593a6084
jongmin yu <jmyu@gist.ac.kr>
2014-11-21 12:53:19 +0900

    experiment for ICML

--- experiment_script.m
+++ experiment_script.m
@@ -3,24 +3,15 @@
 matlabpool open 8;
 
 %% experiment using MNIST and Single layer RBM and Back Propagation
-%mnistclassify_singlelayer;
-
-%% experiment using MNIST and normal RBM and Back Propagation
-%mnistclassify;
-%plot_image('normal_train_error.png',train_err_percentage,(1:max(size(train_err_percentage))),'Train error ratio','epoch','error');
-%plot_image('normal_test_error.png',test_err_percentage,(1:max(size(test_err_percentage))),'Test error ratio','epoch','error');
+mnistclassify;
 
 %% experiment using MINST and RBM with random activation parameter(Alpha) and Back Propagation
-%mnistclassify_randrbm;
-%plot_image('random_train_error.png',train_err_percentage,(1:max(size(train_err_percentage))),'Train error ratio','epoch','error');
-%plot_image('random_train_error.png',test_err_percentage,(1:max(size(test_err_percentage))),'Test error ratio','epoch','error');
-
-
-%% experiment using MINST and RBM with random activation parameter(Alpha) and Back Propagation
-mnistclassify_covrbm;
-%plot_image('random_train_error.png',train_err_percentage,(1:max(size(train_err_percentage))),'Train error ratio','epoch','error');
-%plot_image('random_train_error.png',test_err_percentage,(1:max(size(test_err_percentage))),'Test error ratio','epoch','error');
-
+mnistclassify_strategy1;
+mnistclassify_strategy2;
+mnistclassify_strategy3;
+mnistclassify_strategy4;
+mnistclassify_strategy5;
+mnistclassify_strategy6;
 
 matlabpool close;
 
